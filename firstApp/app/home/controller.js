8f941c4837ebcfc7c07fb3a0f50eb27cc9f0a05d
ykutm <yooutsu@gmail.com>
2017-08-20 11:29:57 +0900

    update text

--- firstApp/app/home/controller.js
+++ firstApp/app/home/controller.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    content: 'this is the some filter content for container.',
+    content: 'this is the some filter content for container',
     h1: 'jumbotron',
     body: 'container',
     spImg: '/assets/passed.png',
